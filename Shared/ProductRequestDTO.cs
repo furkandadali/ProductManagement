a60217c1bbdec124180419daeec58acdf083d335
furkandadali <67693130+furkandadali@users.noreply.github.com>
2021-10-03 11:31:36 +0300

    Add files via upload

---
+++ Shared/ProductRequestDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Shared
+{
+    public class NewProductRequestDTO
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImageLink { get; set; }
+        public decimal ProductPrice { get; set; }
+    } 
+    
+    public class UpdateProductRequestDTO
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImageLink { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ Shared/ProductResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shared
+{
+    public class ProductsResponseDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ProductName { get; set; }
+
+    }
+    public class ProductDetailResponseDTO
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public string ProductImageLink { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductLikeCount { get; set; }
+        public int ProductViewCount { get; set; }
+        public double ProductPoint { get; set; }
+    }
+}
